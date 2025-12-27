c84ed2b42d8c916951f6f8868a11ffb125a8aa17
Your Name <you@example.com>
2025-12-27 21:57:42 +0530

    Added DSA programs

--- src/com/dsa231225/twonumsum.java
+++ src/com/dsa231225/twonumsum.java
@@ -3,15 +3,42 @@ package com.dsa231225;
 import java.util.Scanner;
 
 public class twonumsum {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter 1st num:");
-        int n1 = sc.nextInt();
-        System.out.println("Enter 2nd num:");
-        int n2 = sc.nextInt();
+//    public static void main(String[] args) {
+//        Scanner sc = new Scanner(System.in);
+//        System.out.println("Enter 1st num:");
+//        int n1 = sc.nextInt();
+//        System.out.println("Enter 2nd num:");
+//        int n2 = sc.nextInt();
+//
+//        int sum = n1 + n2;
+//        System.out.println("the sum of no is:" +sum);
+//
+//    }
 
-        int sum = n1 + n2;
-        System.out.println("the sum of no is:" +sum);
-
+/*
+ called the sum many times
+     return_type name(arguments){
+     body
+    return statement
     }
+
+ */
+
+public static void main(String[] args) {
+ sum();
+ sum();
+}
+
+static void sum(){
+  Scanner sc = new Scanner(System.in);
+       System.out.println("Enter 1st num:");
+      int n1 = sc.nextInt();
+      System.out.println("Enter 2nd num:");
+      int n2 = sc.nextInt();
+
+       int sum = n1 + n2;
+       System.out.println("the sum of no is:" +sum);
+
+}
+
 }
